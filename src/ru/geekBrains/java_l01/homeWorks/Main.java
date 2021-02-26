49e41372f6cdc8b9845f3724c32585f4b1023ce2
Mikhail Kosyakov <kosjak@hotmail.com>
2021-02-26 18:43:26 +0300

    Commit after merge with lesson 07

--- src/ru/geekBrains/java_l01/homeWorks/Main.java
+++ src/ru/geekBrains/java_l01/homeWorks/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("this is Main class");
+        int[][] Array = new int[][]{
+                {1,2,3,4},
+                {4,5,6,7}
+        };
+        System.out.println("Rows: " + Array.length);
+        System.out.println("Columns: " + Array[0].length);
+
     }
 }
